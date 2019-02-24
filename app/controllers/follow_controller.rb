4cb204f4f0d4141f06db977bf833090e7a67ad02
Angel <angelpavonperez@gmail.com>
2019-02-24 16:43:05 +0000

    Solution to the follow/unfollow button

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,4 +1,5 @@
 class AccountActivationsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
   
   def create
     user = User.find(params[:followed_id])
@@ -6,8 +7,8 @@ class AccountActivationsController < ApplicationController
     redirect_to user
   end
 
-  def destroy
-    user = Relationship.find(params[:id]).followed
+  def delete
+    #user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
   end
